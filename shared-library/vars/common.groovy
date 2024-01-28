2d55c9ab7aaa69f04f2936266794f741ca98a50a
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-01-28 21:09:52 +0530

    addd

---
+++ shared-library/vars/common.groovy
@@ -0,0 +1,44 @@
+def compile() {
+    stage('Compile Code') {
+    if (env.codeType == "maven") {
+        sh '/home/centos/maven/bin/mvn package'
+    }
+
+    if (env.codeType == "nodejs") {
+        print 'NodeJs'
+    }
+
+    if (env.codeType == "python") {
+        print 'python'
+    }
+
+    if (env.codeType == "static") {
+        print 'static'
+    }
+  }
+}
+
+
+def test() {
+    stage('Test Cases'){
+        print 'Test'
+    }
+}
+
+def codeQuality() {
+    stage('Code Quality'){
+        print 'Code Quality'
+    }
+}
+
+def codeSecurity() {
+    stage('Code Security'){
+        print 'Code Security'
+    }
+}
+
+def release() {
+    stage('Release'){
+        print 'Release'
+    }
+}
