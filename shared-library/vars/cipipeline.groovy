33d619c61bcdaebd80144ad88df7aa9b16ba51d8
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-02-02 16:32:58 +0530

    addd

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -32,7 +32,9 @@ def call() {
       }
 
       stage('Release') {
-
+        when {
+          expression { env.TAG_NAME ==~ ".*" }
+        }
           steps {
             echo 'Hello World'
         }
