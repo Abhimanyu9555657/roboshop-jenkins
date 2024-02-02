ad470fdcd3ed3d52fadcdbfe246c224b8a58d18a
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-02-02 19:09:26 +0530

    changes

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -3,7 +3,7 @@ def call() {
     node('workstation') {
 
         sh "find . | sed -e '1d' |xargs rm -rf "
-        git branch: 'main', url: 'https://github.com/Abhimanyu9555657/${component}'
+        git branch: 'main', url: "https://github.com/Abhimanyu9555657/${component}"
 
         stage('Compile Code') {
             common.compile()
