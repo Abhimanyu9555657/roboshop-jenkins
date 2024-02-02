bef8c908e694837a5cbd6589b075036500fc11b4
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-02-02 16:25:06 +0530

    addd

---
+++ shared-library/vars/cipipeline.groovy
@@ -0,0 +1,42 @@
+def call() {
+  pipeline {
+    agent any
+
+    stages {
+      stage('Compile Code') {
+        steps {
+         sh 'env'
+        }
+      }
+
+      stage('Test') {
+        steps {
+          echo 'Hello World'
+        }
+      }
+
+      stage('Code Quality') {
+        steps {
+          echo 'Hello World'
+        }
+      }
+
+      stage('Code Security') {
+        when {
+          expression { BRANCH_NAME == "main" }
+        }
+
+         steps {
+           echo 'Hello World'
+         }
+      }
+
+      stage('Release') {
+
+          steps {
+            echo 'Hello World'
+        }
+      }
+    }
+  }
+}
